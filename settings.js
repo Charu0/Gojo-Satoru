a886c6132c62116fc8532437bef6e29196861063
Charu <90377378+Charu0@users.noreply.github.com>
2022-06-27 10:20:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94703076694']
+global.premium = ['94703076694']
+global.ownernomer = '94703076694'
+global.ownername = '🐦charu'
+global.botname = 'ᴅͥɪͣʏͫᴀ࿎ ᴮ  ͦ ͭᵛᵉʳˢᶦᵒⁿ ⟁'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.myweb = 'https://sdjg.godaddysites.com/'
+global.packname = 'ᴅͥɪͣʏͫᴀ࿎'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
